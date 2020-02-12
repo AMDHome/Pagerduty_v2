2964fcc421cf578643d27539e3cebbb1ab8bcbd8
Samuel Tseng <amdtseng@ucdavis.edu>
2020-02-11 18:17:09 -0800

    Fixed SSL issues and prepared for adding resolve checking

--- bin/pagerduty.py
+++ bin/pagerduty.py
@@ -4,12 +4,11 @@ import sys
 import json
 import urllib.request
 import urllib.error
-import urllib.parse
-import ssl
+import splunklib.results as results
 
 from fnmatch import fnmatch
 
-SSL_CERT_DIR="/etc/ssl/certs"
+
 
 # generate the JSON for the incident
 # Details: https://v2.developer.pagerduty.com/docs/events-api-v2 
@@ -20,32 +19,43 @@ def generate_inc(details, key):
 
     # determine if triggering or resolving
     eventType = details['search_name'].rsplit(' ', 1)[1]
+
+    # create incident JSON
     if eventType == "resolve":
+        eventType = "resolve"
         details['search_name'] = details['search_name'].rsplit(' ', 1)[0]
     else:
+        # add trigger specific parameters
         eventType = "trigger"
+   
+        payload['summary'] = details['result']['host'] + " " + details['search_name']
+        payload['severity'] = "critical"
+        payload['source'] = details['result']['host']
+        payload['component'] = details['result']['_sourcetype']
+        payload['group'] = details['result']['index']
+        payload['custom_details'] = details['result']
 
-    # create JSON Payload
-    payload['summary'] = details['result']['host'] + " " + details['search_name']
-    payload['severity'] = "critical"
-    payload['source'] = details['result']['host']
-    payload['component'] = details['result']['_sourcetype']
-    payload['group'] = details['result']['index']
-    payload['custom_details'] = details['result']
+        # create incident JSON
+        inc['payload'] = payload
+        inc['client'] = "Splunk"
+        inc['client_url'] = details['results_link']
 
+    # computer dedup_key
     dedup_key = details['search_name'] + " (Server: " + details['result']['host'] + ")"
     if len(dedup_key) > 255:
         dedup_key = dedup_key[0 : 254]
 
-    # create incident JSON
-    inc['payload'] = payload
+    # add general incident parameters
     inc['routing_key'] = key
     inc['dedup_key'] = dedup_key
     inc['event_action'] = eventType
-    inc['client'] = "Splunk"
-    inc['client_url'] = details['results_link']
 
     return inc
+
+
+
+def resolved_checker(inc):
+    return
 
 
 
@@ -74,20 +84,22 @@ def send_notification(details):
     eventType = inc['event_action']
     body = json.dumps(inc).encode("utf-8")
 
-    #Ignore Certs
-    ssl._create_default_https_context = ssl._create_unverified_context
+    #if inc['event_action'] == "resolve":
+        #check splunk for last event
 
+    # Ignore Certs
+    #ssl._create_default_https_context = ssl._create_unverified_context
 
     # send PagerDuty incident info
     print('DEBUG Calling url="{:s}" with body={:s}'.format(url, body.decode()), file=sys.stderr)
     req = urllib.request.Request(url=url, data=body, headers={"Content-Type": "application/json"})
 
     try:
-        res = urllib.request.urlopen(req)
+        res = urllib.request.urlopen(req, cafile="/etc/pki/tls/certs/ca-bundle.crt")
         body = res.read().decode("utf-8")
         print("INFO PagerDuty server responded with HTTP status = {:d}".format(res.code), file=sys.stderr)
         print("DEBUG PagerDuty server response: {:s}".format(body), file=sys.stderr)
-        return 200 <= res.code < 300
+        return 200 <= res.code < 300, inc['event_action'], inc['dedup_key']
     except urllib.error.HTTPError as e:
         print("ERROR Error sending message: {:s} ({:s})".format(e, str(dir(e))), file=sys.stderr)
         print("ERROR Server response: {:s}".format(e.read()), file=sys.stderr)
@@ -97,17 +109,18 @@ def send_notification(details):
 
 if __name__ == "__main__":
 
-    sys.stdout = open('/tmp/output', 'w')
-    sys.stderr = open('/tmp/output', 'w')
+    # sys.stdout = open('/tmp/output', 'w')
+    # sys.stderr = open('/tmp/output', 'w')
 
     if len(sys.argv) > 1 and sys.argv[1] == "--execute":
         payload = json.loads(sys.stdin.read())
-        success = send_notification(payload)
+        success, action, dedup_key = send_notification(payload)
         if not success:
             print("FATAL Failed trying to incident alert", file=sys.stderr)
             sys.exit(2)
         else:
-            print("INFO Incident alert notification successfully sent", file=sys.stderr)
+            print("INFO {:s} {:s} successfully sent".format(dedup_key, action), file=sys.stderr)
+            
     else:
         print("FATAL Unsupported execution mode (expected --execute flag)", file=sys.stderr)
         sys.exit(1)
